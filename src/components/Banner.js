0707371164c841aa8c0d211a0519973d7ad6cbd6
nvchien2811 <nvchien2811@gmail.com>
2022-05-08 10:19:45 +0700

    old

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,7 +12,7 @@ function Banner(){
             </div>
             <div className="right">
                 <div className="animate__animated animate__rubberBand hello">
-                    <h2>Helloooooooooooooooo!</h2>
+                    <h2>Hello!</h2>
                 </div>
                 <h3>I'm Chien, a student of VKU.</h3>
                 <button className="btn_readmore" onClick={scrollToProfile}>
